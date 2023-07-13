10ac617c322c0f6e09c873188dbffd728fd7a863
Vladimir <troyanskii1998@yandex.ru>
2023-07-14 00:20:14 +0300

    добавляет базовые маршруты в приложении, добавляет страницу 404

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,19 @@
-const BASEDATA = {
+export const BASEDATA = {
   userLogin: 'Oliver.conner@gmail.com',
   favoriteHotelsCount: 3,
   currentOffersInCity: 3,
   currentCityName: 'Amsterdam',
 } as const;
 
-export default BASEDATA;
+export enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer',
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,3 +1,4 @@
+import Logo from '../../components/logo/logo';
 import PlaceCard from '../../components/place-card/place-card';
 
 const BASEHOTELSCOUNT = 5;
@@ -15,17 +16,7 @@ function MainPage({userLogin, favoriteHotelsCount, currentOffersInCity, currentC
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
-                <img
-                  className="header__logo"
-                  src="img/logo.svg"
-                  alt="6 cities logo"
-                  width={81}
-                  height={41}
-                />
-              </a>
-            </div>
+            <Logo />
             <nav className="header__nav">
               <ul className="header__nav-list">
                 <li className="header__nav-item user">
